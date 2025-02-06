b25ac8069dadb68c5e5be7791670ceb3ad77af2b
ElAaronNos <moracontrerasaaron6@gmail.com>
2025-02-06 11:34:47 -0400

    Add files via upload

--- Mini Tienda.py
+++ Mini Tienda.py
@@ -1,10 +1,6 @@
 items = [] #Lista
-Oprecio = 0 #Precio Original
+precio = [] #Precio
 num_items = 1 #Numeros de los items
-a = 1
-b = 2
-c = 3
-d = 4
 
 def clear():
  print("")
@@ -33,12 +29,12 @@ import random #Importa el randomizador
 print("Hola bienvenido a la tienda :) escriba una opcion con los numeros o las letras que le indique")
 
 #Codigo
-while True: #Codigo
+while True:
  print("----------------------------------------------------------------------------------------------------------------")
- print("[1]Agregar compra") #Debe dar una opcion en articulo y precio y en el output mostrar el articulo
- print("[2]Borrar producto")
+ print("[1]Agregar compra")
+ print("[2]Borrar un producto")
  print("[3]Ver productos")
- print("[4]Total")
+ print("[4]Total de la compra")
  print("[5]Salir")
  print("----------------------------------------------------------------------------------------------------------------")
  num = int(input()) #Opcion Elegida
@@ -46,7 +42,10 @@ while True: #Codigo
  if num == 1: #¿Que desea comprar?
   print("¿que deseas llevar?")
   select = input()
+  print("¿Cual es el precio de la compra?")
+  precio_a = float(input())
   items.append(select) #Agrega
+  precio.append(precio_a)
   if num_items == 1:
    print(f"tiene en el carrito: 1 producto")
    time.sleep(1.5)
@@ -58,37 +57,41 @@ while True: #Codigo
   clear()
 
  elif num == 2:
-  print(f"¿Esta seguro que desea borrar su lista de compras, tienes:{items}? Y/N")
-  ver = input()
-  if ver == "Y": #Si
-      items.remove(select)
-      num_items = 0
-      print("Se ah borrado la lista")
-      time.sleep(2)
-      clear()
-  else: #No
-      print("No se ah eliminado nada")
-      time.sleep(2)
-      clear()
-      
+  if num_items > 1:
+   print(f"¿Que desea borrar?")
+   select = input()
+   print(f"¿Esta seguro que desea borrar {select} de su lista? Y/N")
+   ver = input()
+   if ver == "Y": #Si
+       items.remove(select)
+       num_items -= 1
+       print(f"Se ah borrado {select}")
+       time.sleep(2)
+       clear()
+   else: #No
+       print("No se ah eliminado nada")
+       time.sleep(2)
+       clear()
+  else:
+    print("No tiene nada en el carrito")
+    time.sleep(2)
+    clear()
+
  elif num == 3: #Cuantos Items tengo
-     print(f"Usted Tiene: {items}")
-     time.sleep(3)
-     clear()
-     
- elif num == 4: #total
-     randomer = random.randint(1, 4)
-     if randomer == 1:
-         a *= Oprecio
-     elif randomer == 2:
-         b *= Oprecio
-     elif randomer == 3:
-         c *= Oprecio
+     if num_items > 1:
+      print(f"Usted Tiene: {items}")
+      time.sleep(3)
+      clear()
      else:
-         d *= Oprecio 
-     print(f"El precio total de su carrito:{Oprecio}")
-     time.sleep(3)
-     clear()
+      print("No tiene nada en el carrito") 
+ elif num == 4: #total
+     if num_items > 1:
+      Fprecio = sum(precio)
+      print(f"El precio total de su carrito:{Fprecio}$")
+      time.sleep(3)
+      clear()
+     else: 
+      print("No tiene nada en el carrito") 
      
  elif num == 5: #Exit
      print("Gracias Por Comprar :)")
